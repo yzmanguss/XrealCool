add2a305dde195372e9b458607d1c79f22ebbe8e
杨忠明 <1720584431@qq.com>
2019-11-27 22:47:15 +0800

    数据库创建和配置

--- app/src/main/java/com/example/xrealcool/android/MainActivity.java
+++ app/src/main/java/com/example/xrealcool/android/MainActivity.java
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * 获取全区天气情况
+ *  创建数据库和表
+ *      1.创建省，市，县的类  ---->   2.创建assets包，创建litepal.xml   -----> 3.在AndroidManifest里面配置android:name="org.litepal.LitePalApplication"
+ *
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
